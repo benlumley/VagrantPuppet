f38649c9783ce0829a3d75602d5b337d68027cc9
Ben Lumley <ben@benlumley.co.uk>
2012-05-03 12:36:40 +0100

    prototype apache2 module

---
+++ apache2/manifests/debian.pp
@@ -0,0 +1,24 @@
+# apache2/manifests/classes/apache2_debian.pp
+# by Sam Quigley <sq@wesabe.com>
+
+# this class attempts to mimic the default configuration of the debian
+# apache2 package
+
+class apache2::debian inherits apache2 {
+  apache2::config { "debian":
+    order => "000",
+    ensure => present,
+  }
+
+  apache2::module { ["alias", "auth_basic", "authn_file", 
+    "authz_default", "authz_groupfile", "authz_host", 
+    "authz_user", "autoindex", "cgid", "dir", "env", "mime", 
+    "negotiation", "setenvif", "status", "rewrite", "actions" ]:
+    ensure=> present,
+  }
+
+  apache2::module { 'fastcgi': 
+    require => PAckage['libapache2-mod-fastcgi'],
+    ensure => 'present'
+  }
+}
