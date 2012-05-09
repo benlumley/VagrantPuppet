bc2f5475a48fa15054b6dedf2b8d18b8d3de33e2
Ben Lumley <ben@benlumley.co.uk>
2012-05-09 03:43:36 -0700

    Postfix module

---
+++ postfix/manifests/init.pp
@@ -0,0 +1,12 @@
+class postfix {
+  package { 'postfix':
+    ensure => 'present'
+  }
+
+  service { 'postfix':
+    ensure => running,
+    enable => true,
+    require => Package['postfix'],
+  }
+
+}
